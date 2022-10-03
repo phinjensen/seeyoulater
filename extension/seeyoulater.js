fe7869a4399c90d5a07bd04af086d8ded085f012
Phineas Jensen <phin@endpointdev.com>
2022-10-02 20:08:07 -0600

    Add notification support to extension

--- extension/seeyoulater.js
+++ extension/seeyoulater.js
@@ -1,18 +1,38 @@
 browser.runtime.onMessage.addListener(
   async ({ addBookmark: { title, description, url, tags } }) => {
-    let { server_url } = await browser.storage.sync.get("server_url");
-    let response = await fetch(server_url + "/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        url,
-        description,
-        tags,
-      }),
+    let notificationId = await browser.notifications.create({
+      type: "basic",
+      title: "See You Later",
+      message: `Sending bookmark to server...`,
     });
-    let body = await response.json();
+    let { server_url } = await browser.storage.sync.get("server_url");
+    try {
+      let response = await fetch(server_url + "/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          url,
+          description,
+          tags,
+        }),
+      });
+      let body = await response.json();
+      await browser.notifications.clear(notificationId);
+      notificationId = await browser.notifications.create({
+        type: "basic",
+        title: "See You Later",
+        message: `Saved bookmark successfully`,
+      });
+    } catch (err) {
+      await browser.notifications.clear(notificationId);
+      notificationId = await browser.notifications.create({
+        type: "basic",
+        title: `Error saving bookmark: ${err}`,
+        message: `Saved bookmark successfully`,
+      });
+    }
   }
 );
